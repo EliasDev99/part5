af7dbf39b992be00c042dcc24fda2dd01d56a58f
unknown <elias.dev99@gmail.com>
2025-11-05 10:33:09 +0200

    Add 5.2: Blog List Frontend, step 2

--- src/App.jsx
+++ src/App.jsx
@@ -17,10 +17,21 @@ const App = () => {
     )  
   }, [])
 
+   useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogAppUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
+  }, [])
+
    const handleLogin = async (username, password) => {
 
     try {
       const user = await loginService.login({ username, password })
+      window.localStorage.setItem(
+        'loggedBlogAppUser', JSON.stringify(user)
+      )
       setUser(user)
     } catch {
       setErrorMessage('wrong credentials')
@@ -28,6 +39,11 @@ const App = () => {
         setErrorMessage(null)
       }, 5000)
     }
+  }
+  
+   const handleLogout =  () => {
+    window.localStorage.removeItem('loggedBlogAppUser')
+    setUser(null)
   }
 
   if(user === null) {
@@ -43,7 +59,10 @@ const App = () => {
     <div>
       <Notification message={errorMessage} />
       <h2>Blogs</h2>
-      <p>{user.name} logged in</p>
+      <p>
+        {user.name} logged in
+        <button onClick={handleLogout}>logout</button>
+      </p>
        {blogs.map(blog => <Blog key={blog.id} blog={blog} /> )}
     </div>
   )
