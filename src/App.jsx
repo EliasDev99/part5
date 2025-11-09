801b0b846ee8a2f77d7c0876822d144254a3979d
unknown <elias.dev99@gmail.com>
2025-11-09 10:08:54 +0200

    Add 5.5 Blog List Frontend, step 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 import Notification from './components/Notification'
 import LoginForm from './components/LoginForm'
 import loginService from './services/login'
 import BlogForm from './components/BlogForm'
+import Togglable from './components/Togglable'
 
 const App = () => {
   const [notification, setNotification] = useState({ message: null, type: '' })
   const [blogs, setBlogs] = useState([])
   const [user, setUser] = useState(null)
+
+  const blogFormRef = useRef()
 
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs))
@@ -49,12 +52,12 @@ const App = () => {
 
   const handleCreateBlog = async (newBlog) => {
     const response = await blogService.create(newBlog)
-    console.log(response)
     setBlogs(blogs.concat(response))
     showNotification(
       `a new blog, ${response.title} by ${response.author}`,
       'success'
     )
+    blogFormRef.current.toggleVisibility() // hide the form after created
   }
 
   if (user === null) {
@@ -74,7 +77,11 @@ const App = () => {
         {user.name} logged in
         <button onClick={handleLogout}>logout</button>
       </p>
-      <BlogForm onCreate={handleCreateBlog} />
+
+      <Togglable buttonLabel="create new blog" ref={blogFormRef}>
+        <BlogForm onCreate={handleCreateBlog} />
+      </Togglable>
+
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
