5af24b103be60eaa4ee2154adbb719df5318be13
unknown <elias.dev99@gmail.com>
2025-11-13 21:31:39 +0200

    Add 5.15: Blog List Tests, step 3

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -1,10 +1,24 @@
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
-import { test, describe, expect, beforeEach } from 'vitest'
+import { test, describe, expect, beforeEach, vi } from 'vitest'
+import blogService from '../services/blogs'
+
+vi.mock('../services/blogs', () => {
+  return {
+    default: {
+      update: vi.fn((id, blog) => Promise.resolve({ ...blog, id })),
+      getAll: vi.fn(),
+      create: vi.fn(),
+      remove: vi.fn(),
+      setToken: vi.fn(),
+    },
+  }
+})
 
 describe('<Blog />', () => {
   const blog = {
+    id: '2025fullstack',
     title: 'Testing react app ',
     author: 'Matti',
     url: 'https://fullstackhelsinki',
@@ -12,13 +26,13 @@ describe('<Blog />', () => {
     user: { username: 'juhamatti', name: 'Juha', id: '123' },
   }
 
-  const user = { username: 'juhamatti', name: 'Juha' }
-
+  const userInfo = { username: 'juhamatti', name: 'Juha' }
   beforeEach(() => {
-    render(<Blog blog={blog} user={user} />)
+    vi.clearAllMocks()
   })
 
   test('show title and author, but not url or likes by default', () => {
+    render(<Blog blog={blog} user={userInfo} onLike={vi.fn()} />)
     const element = screen.getByText('Testing react app Matti')
     expect(element).toBeVisible()
 
@@ -30,6 +44,7 @@ describe('<Blog />', () => {
   })
 
   test('likes and url shown after clicking the view button', async () => {
+    render(<Blog blog={blog} user={userInfo} onLike={vi.fn()} />)
     const user = userEvent.setup()
 
     const viewButton = screen.getByText('view')
@@ -37,5 +52,21 @@ describe('<Blog />', () => {
 
     expect(screen.getByText('https://fullstackhelsinki')).toBeVisible()
     expect(screen.getByText('likes 10')).toBeVisible()
+  })
+
+  test('like button is clicked twice', async () => {
+    const userEve = userEvent.setup()
+    const likeHandler = vi.fn()
+
+    render(<Blog blog={blog} user={userInfo} onLike={likeHandler} />)
+
+    const viewButton = screen.getAllByText('view')[0]
+    await userEve.click(viewButton)
+
+    const likeButton = screen.getByText('like')
+    await userEve.click(likeButton)
+    await userEve.click(likeButton)
+
+    expect(likeHandler.mock.calls).toHaveLength(2)
   })
 })
