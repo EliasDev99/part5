cb7716a4f375ce72091bbf03389c358f503b28ec
unknown <elias.dev99@gmail.com>
2025-11-13 18:14:52 +0200

    Add 5.13: Blog List Tests, step 1

---
+++ src/components/Blog.test.jsx
@@ -0,0 +1,30 @@
+import { render, screen } from '@testing-library/react'
+import Blog from './Blog'
+import { test, describe, expect, beforeEach } from 'vitest'
+
+describe('<Blog />', () => {
+  const blog = {
+    title: 'Testing react app ',
+    author: 'Matti',
+    url: 'https://fullstackhelsinki',
+    likes: 10,
+    user: { username: 'juhamatti', name: 'Juha', id: '123' },
+  }
+
+  const user = { username: 'juhamatti', name: 'Juha' }
+
+  beforeEach(() => {
+    render(<Blog blog={blog} user={user} />)
+  })
+
+  test('show title and author, but not url or likes by default', () => {
+    const element = screen.getByText('Testing react app Matti')
+    expect(element).toBeVisible()
+
+    const showDetails = screen.queryByText('https://fullstackhelsinki')
+    expect(showDetails).toBeNull()
+
+    const likes = screen.queryByText('likes')
+    expect(likes).toBeNull()
+  })
+})
